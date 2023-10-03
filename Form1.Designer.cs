25c2e7073ddf3de9fd9499cd43cef99f0a44e63f
andre <andrew260602sush@gmail.com>
2023-10-03 22:01:58 +0300

    Линии мои любимые

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
+            Canvas = new PictureBox();
+            Button1 = new Button();
+            Button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)Canvas).BeginInit();
             SuspendLayout();
+            // 
+            // Canvas
+            // 
+            Canvas.BackColor = SystemColors.Window;
+            Canvas.Location = new Point(0, 0);
+            Canvas.Margin = new Padding(3, 2, 3, 2);
+            Canvas.Name = "Canvas";
+            Canvas.Size = new Size(724, 407);
+            Canvas.TabIndex = 0;
+            Canvas.TabStop = false;
+            Canvas.Click += Canvas_Click;
+            // 
+            // Button1
+            // 
+            Button1.Location = new Point(771, 20);
+            Button1.Margin = new Padding(3, 2, 3, 2);
+            Button1.Name = "Button1";
+            Button1.Size = new Size(118, 27);
+            Button1.TabIndex = 1;
+            Button1.Text = "Рисовать";
+            Button1.UseVisualStyleBackColor = true;
+            Button1.Click += Button1_Click;
+            // 
+            // Button2
+            // 
+            Button2.Location = new Point(771, 72);
+            Button2.Margin = new Padding(3, 2, 3, 2);
+            Button2.Name = "Button2";
+            Button2.Size = new Size(118, 27);
+            Button2.TabIndex = 2;
+            Button2.Text = "Очистить";
+            Button2.UseVisualStyleBackColor = true;
+            Button2.Click += Button2_Click;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1014, 546);
+            ClientSize = new Size(931, 406);
+            Controls.Add(Button2);
+            Controls.Add(Button1);
+            Controls.Add(Canvas);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Графика. Лабораторная 2";
+            ((System.ComponentModel.ISupportInitialize)Canvas).EndInit();
             ResumeLayout(false);
         }
 
+
+
         #endregion
+
+        private PictureBox Canvas;
+        private Button Button1;
+        private Button Button2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,125 @@
+﻿using System.Data;
+
 namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
+        private DrawingState _drawingState;
+        DrawingObject _drawingObject = new();
+        Pen _pen;
+        private Graphics g;
+
         public Form1()
         {
             InitializeComponent();
+            g = Canvas.CreateGraphics();
+            _pen = new Pen(Color.Black, 1);
         }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            _drawingState = DrawingState.FREE_DROWING;
+        }
+        private void FreeDraw(PointF p)
+        {
+            if (_drawingObject.IsPoligon())
+            {
+                _drawingObject.Clear();
+                g.Clear(Color.White);
+            }
+            if (_drawingObject.IsClosable(p))
+            {
+                PointF prev = _drawingObject.LastDot;
+                _drawingObject.Close();
+                g.DrawLine(_pen, prev, _drawingObject.Lines.First.Value.a);
+            }
+            else
+            {
+                if (_drawingObject.IsEmpty())
+                {
+                    g.DrawRectangle(_pen, p.X, p.Y, 1, 1);
+                }
+                else
+                {
+                    PointF prev = _drawingObject.LastDot;
+                    g.DrawLine(_pen, prev, p);
+
+                }
+                _drawingObject.AddDot(p);
+            }
+        }
+
+        private void Canvas_Click(object sender, EventArgs e)
+        {
+            if (_drawingState == DrawingState.FREE_DROWING)
+            {
+                MouseEventArgs mouseEventArgs = (MouseEventArgs)e;
+                Point p = new Point(mouseEventArgs.X, mouseEventArgs.Y);
+                FreeDraw(p);
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            _drawingState = DrawingState.NODROWING;
+            g.Clear(Color.White);
+            _drawingObject.Clear();
+        }
+
+        enum DrawingState { NODROWING, FREE_DROWING, DROWING_DOT }
+    }
+
+    class DrawingObject
+    {
+        public const int DIST_FOR_CONNECTING = 10;
+        public PointF LastDot;
+        public LinkedList<Line> Lines = new();
+        public DrawingObject()
+        {
+            LastDot = Point.Empty;
+        }
+        public void AddDot(PointF p)
+        {
+            if (!LastDot.Equals(PointF.Empty))
+            {
+                Lines.AddLast(new Line(LastDot, p));
+            }
+            LastDot = p;
+        }
+
+
+
+        public bool IsEmpty() => LastDot.Equals(PointF.Empty);
+
+        public bool IsDot() => Lines.Count == 0 && !LastDot.Equals(PointF.Empty);
+
+        public bool IsLine() => Lines.Count == 1;
+
+        public bool IsCurve() => Lines.Count > 0 && !Lines.First.Value.a.Equals(Lines.Last.Value.b);
+
+        public bool IsPoligon() => Lines.Count > 0 && Lines.First.Value.a.Equals(Lines.Last.Value.b);
+
+
+        public bool IsClosable(PointF p) => Lines.Count > 0 && DIST_FOR_CONNECTING >= Math.Sqrt(Math.Pow(p.X - Lines.First.Value.a.X, 2) +
+            Math.Pow(p.Y - Lines.First.Value.a.Y, 2));
+        public bool Close()
+        {
+            if (IsEmpty() || IsDot() || IsLine()) return false;
+            AddDot(Lines.First.Value.a);
+            return true;
+        }
+
+        public void Clear()
+        {
+            LastDot = PointF.Empty;
+            Lines.Clear();
+        }
+    }
+
+    class Line
+    {
+        public PointF a;
+        public PointF b;
+        public Line(PointF a, PointF b) { this.a = a; this.b = b; }
     }
 }
