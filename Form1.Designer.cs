a4ebef8cf323987c7c9d8899810642f31dae36fd
Allan <53708451+FrenchIceCream@users.noreply.github.com>
2023-10-07 20:13:27 +0300

    Аффинные преобразования

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,6 +31,20 @@
             Canvas = new PictureBox();
             Button1 = new Button();
             Button2 = new Button();
+            Button_SetPoint = new Button();
+            Button_Turn = new Button();
+            Button_Scale = new Button();
+            dx = new TextBox();
+            dy = new TextBox();
+            dx_label = new Label();
+            dy_label = new Label();
+            Button_Move = new Button();
+            degree = new TextBox();
+            kx = new TextBox();
+            ky = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
             ((System.ComponentModel.ISupportInitialize)Canvas).BeginInit();
             SuspendLayout();
             // 
@@ -38,19 +52,17 @@
             // 
             Canvas.BackColor = SystemColors.Window;
             Canvas.Location = new Point(0, 0);
-            Canvas.Margin = new Padding(3, 2, 3, 2);
             Canvas.Name = "Canvas";
-            Canvas.Size = new Size(724, 407);
+            Canvas.Size = new Size(827, 580);
             Canvas.TabIndex = 0;
             Canvas.TabStop = false;
             Canvas.Click += Canvas_Click;
             // 
             // Button1
             // 
-            Button1.Location = new Point(771, 20);
-            Button1.Margin = new Padding(3, 2, 3, 2);
+            Button1.Location = new Point(881, 27);
             Button1.Name = "Button1";
-            Button1.Size = new Size(118, 27);
+            Button1.Size = new Size(135, 36);
             Button1.TabIndex = 1;
             Button1.Text = "Рисовать";
             Button1.UseVisualStyleBackColor = true;
@@ -58,28 +70,162 @@
             // 
             // Button2
             // 
-            Button2.Location = new Point(771, 72);
-            Button2.Margin = new Padding(3, 2, 3, 2);
+            Button2.Location = new Point(881, 96);
             Button2.Name = "Button2";
-            Button2.Size = new Size(118, 27);
+            Button2.Size = new Size(135, 36);
             Button2.TabIndex = 2;
             Button2.Text = "Очистить";
             Button2.UseVisualStyleBackColor = true;
             Button2.Click += Button2_Click;
             // 
+            // Button_SetPoint
+            // 
+            Button_SetPoint.Location = new Point(881, 204);
+            Button_SetPoint.Name = "Button_SetPoint";
+            Button_SetPoint.Size = new Size(135, 35);
+            Button_SetPoint.TabIndex = 3;
+            Button_SetPoint.Text = "Задать точку";
+            Button_SetPoint.UseVisualStyleBackColor = true;
+            Button_SetPoint.Click += Button_SetPoint_Click;
+            // 
+            // Button_Turn
+            // 
+            Button_Turn.Location = new Point(881, 286);
+            Button_Turn.Name = "Button_Turn";
+            Button_Turn.Size = new Size(135, 38);
+            Button_Turn.TabIndex = 4;
+            Button_Turn.Text = "Поворот";
+            Button_Turn.UseVisualStyleBackColor = true;
+            Button_Turn.Click += Button_Turn_Click;
+            // 
+            // Button_Scale
+            // 
+            Button_Scale.Location = new Point(881, 330);
+            Button_Scale.Name = "Button_Scale";
+            Button_Scale.Size = new Size(135, 38);
+            Button_Scale.TabIndex = 5;
+            Button_Scale.Text = "Масштаб";
+            Button_Scale.UseVisualStyleBackColor = true;
+            Button_Scale.Click += Button_Scale_Click;
+            // 
+            // dx
+            // 
+            dx.Location = new Point(881, 403);
+            dx.Name = "dx";
+            dx.Size = new Size(59, 27);
+            dx.TabIndex = 6;
+            // 
+            // dy
+            // 
+            dy.Location = new Point(957, 403);
+            dy.Name = "dy";
+            dy.Size = new Size(59, 27);
+            dy.TabIndex = 7;
+            // 
+            // dx_label
+            // 
+            dx_label.AutoSize = true;
+            dx_label.Location = new Point(896, 380);
+            dx_label.Name = "dx_label";
+            dx_label.Size = new Size(25, 20);
+            dx_label.TabIndex = 8;
+            dx_label.Text = "dx";
+            // 
+            // dy_label
+            // 
+            dy_label.AutoSize = true;
+            dy_label.Location = new Point(973, 380);
+            dy_label.Name = "dy_label";
+            dy_label.Size = new Size(25, 20);
+            dy_label.TabIndex = 9;
+            dy_label.Text = "dy";
+            // 
+            // Button_Move
+            // 
+            Button_Move.Location = new Point(881, 245);
+            Button_Move.Name = "Button_Move";
+            Button_Move.Size = new Size(135, 35);
+            Button_Move.TabIndex = 10;
+            Button_Move.Text = "Смещение";
+            Button_Move.UseVisualStyleBackColor = true;
+            Button_Move.Click += Button_Move_Click;
+            // 
+            // degree
+            // 
+            degree.Location = new Point(957, 454);
+            degree.Name = "degree";
+            degree.Size = new Size(59, 27);
+            degree.TabIndex = 11;
+            // 
+            // kx
+            // 
+            kx.Location = new Point(881, 507);
+            kx.Name = "kx";
+            kx.Size = new Size(59, 27);
+            kx.TabIndex = 12;
+            // 
+            // ky
+            // 
+            ky.Location = new Point(957, 507);
+            ky.Name = "ky";
+            ky.Size = new Size(59, 27);
+            ky.TabIndex = 13;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(973, 484);
+            label1.Name = "label1";
+            label1.Size = new Size(23, 20);
+            label1.TabIndex = 15;
+            label1.Text = "ky";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(896, 484);
+            label2.Name = "label2";
+            label2.Size = new Size(23, 20);
+            label2.TabIndex = 14;
+            label2.Text = "kx";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(888, 457);
+            label3.Name = "label3";
+            label3.Size = new Size(44, 20);
+            label3.TabIndex = 16;
+            label3.Text = "Угол:";
+            label3.TextAlign = ContentAlignment.TopCenter;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(931, 406);
+            ClientSize = new Size(1056, 577);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Controls.Add(label2);
+            Controls.Add(ky);
+            Controls.Add(kx);
+            Controls.Add(degree);
+            Controls.Add(Button_Move);
+            Controls.Add(dy_label);
+            Controls.Add(dx_label);
+            Controls.Add(dy);
+            Controls.Add(dx);
+            Controls.Add(Button_Scale);
+            Controls.Add(Button_Turn);
+            Controls.Add(Button_SetPoint);
             Controls.Add(Button2);
             Controls.Add(Button1);
             Controls.Add(Canvas);
-            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
-            Text = "Графика. Лабораторная 2";
+            Text = "Графика. Лабораторная 3";
             ((System.ComponentModel.ISupportInitialize)Canvas).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
 
@@ -89,5 +235,19 @@
         private PictureBox Canvas;
         private Button Button1;
         private Button Button2;
+        private Button Button_SetPoint;
+        private Button Button_Turn;
+        private Button Button_Scale;
+        private TextBox dx;
+        private TextBox dy;
+        private Label dx_label;
+        private Label dy_label;
+        private Button Button_Move;
+        private TextBox degree;
+        private TextBox kx;
+        private TextBox ky;
+        private Label label1;
+        private Label label2;
+        private Label label3;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,24 +1,37 @@
 ﻿using System.Data;
+using System.Diagnostics;
+using System.Drawing.Drawing2D;
+using System.Numerics;
+using static System.Windows.Forms.LinkLabel;
 
 namespace WinFormsApp1
 {
+    using static System.Drawing.Drawing2D.Matrix;
+    using static System.Net.Mime.MediaTypeNames;
+
     public partial class Form1 : Form
     {
         private DrawingState _drawingState;
         DrawingObject _drawingObject = new();
         Pen _pen;
         private Graphics g;
+        Point helper;
 
         public Form1()
         {
             InitializeComponent();
             g = Canvas.CreateGraphics();
             _pen = new Pen(Color.Black, 1);
+            dx.Text = "10";
+            dy.Text = "10";
+            kx.Text = "0,5";
+            ky.Text = "0,5";
+            degree.Text = "90";
         }
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            _drawingState = DrawingState.FREE_DROWING;
+            _drawingState = DrawingState.FREE_DRAWING;
         }
         private void FreeDraw(PointF p)
         {
@@ -43,7 +56,6 @@ namespace WinFormsApp1
                 {
                     PointF prev = _drawingObject.LastDot;
                     g.DrawLine(_pen, prev, p);
-
                 }
                 _drawingObject.AddDot(p);
             }
@@ -51,22 +63,198 @@ namespace WinFormsApp1
 
         private void Canvas_Click(object sender, EventArgs e)
         {
-            if (_drawingState == DrawingState.FREE_DROWING)
+            if (_drawingState == DrawingState.FREE_DRAWING)
             {
                 MouseEventArgs mouseEventArgs = (MouseEventArgs)e;
                 Point p = new Point(mouseEventArgs.X, mouseEventArgs.Y);
                 FreeDraw(p);
             }
+            else if (_drawingState == DrawingState.DRAWING_DOT)
+            {
+                MouseEventArgs mouseEventArgs = (MouseEventArgs)e;
+                helper = new Point(mouseEventArgs.X, mouseEventArgs.Y);
+            }
         }
 
         private void Button2_Click(object sender, EventArgs e)
         {
-            _drawingState = DrawingState.NODROWING;
+            _drawingState = DrawingState.NODRAWING;
             g.Clear(Color.White);
             _drawingObject.Clear();
+            helper = Point.Empty;
         }
 
-        enum DrawingState { NODROWING, FREE_DROWING, DROWING_DOT }
+        private void Button_Move_Click(object sender, EventArgs e)
+        {
+            if (!_drawingObject.IsPoligon())
+            {
+                g.Clear(Color.White);
+                return;
+            }
+
+            float dist_x = float.Parse(dx.Text);
+            float dist_y = float.Parse(dy.Text);
+            List<float[,]> dot_list = new List<float[,]>();
+            List<Point> dot_list_new = new List<Point>();
+
+            //формируем матрицу-строку координат и матрицу преобразования
+            for (int i = 0; i < _drawingObject.Lines.Count; i++)
+                dot_list.Add(new float[1, 3] { { _drawingObject.Lines.ElementAt(i).a.X, _drawingObject.Lines.ElementAt(i).a.Y, 1 } });
+            dot_list.Add(new float[1, 3] { { _drawingObject.Lines.Last().b.X, _drawingObject.Lines.Last().b.Y, 1 } });
+
+            float[,] move_matrix = new float[3, 3] { { 1, 0, 0 }, { 0, 1, 0 }, { dist_x, dist_y, 1 } };
+            float temp = 0;
+            float[,] m = new float[3, 1];
+
+            //перемножаем с матрицей
+            for (int p = 0; p < dot_list.Count; p++)
+            {
+                for (int i = 0; i < 1; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        temp = 0;
+                        for (int k = 0; k < 3; k++)
+                        {
+                            temp += dot_list.ElementAt(p)[i, k] * move_matrix[k, j];
+                        }
+                        m[j, 0] = temp;
+                    }
+                }
+                dot_list_new.Add(new Point((int)m[0, 0], (int)m[1, 0]));
+            }
+
+            g.Clear(Color.White);
+            Point prev = dot_list_new.First();
+            for (int i = 0; i < dot_list_new.Count; i++)
+            {
+                g.DrawLine(_pen, prev, dot_list_new.ElementAt(i));
+                prev = dot_list_new.ElementAt(i);
+            }
+        }
+
+        private void Button_Turn_Click(object sender, EventArgs e)
+        {
+            if (!_drawingObject.IsPoligon())
+            {
+                g.Clear(Color.White);
+                return;
+            }
+            float deg = float.Parse(degree.Text) % 360;
+            List<float[,]> dot_list = new List<float[,]>();
+            List<Point> dot_list_new = new List<Point>();
+
+            float dist_x = 0;
+            float dist_y = 0;
+            //формируем матрицу-строку координат и матрицу преобразования
+            for (int i = 0; i < _drawingObject.Lines.Count; i++)
+            {
+                dot_list.Add(new float[1, 3] { { _drawingObject.Lines.ElementAt(i).a.X, _drawingObject.Lines.ElementAt(i).a.Y, 1 } });
+                dist_x += _drawingObject.Lines.ElementAt(i).a.X;
+                dist_y += _drawingObject.Lines.ElementAt(i).a.Y;
+            }
+            dist_x = helper.IsEmpty ? dist_x / dot_list.Count : helper.X;
+            dist_y = helper.IsEmpty ? dist_y / dot_list.Count : helper.Y;
+            dot_list.Add(new float[1, 3] { { _drawingObject.Lines.Last().b.X, _drawingObject.Lines.Last().b.Y, 1 } });
+
+            float[,] deg_matrix = new float[3, 3] { {  (float)Math.Cos(deg), (float)Math.Sin(deg), 0 },
+                                                    { -(float)Math.Sin(deg), (float)Math.Cos(deg), 0 },
+            { (float)(-dist_x*Math.Cos(deg) + dist_y*Math.Sin(deg) + dist_x), (float)(-dist_x*Math.Sin(deg) - dist_y*Math.Cos(deg) + dist_y), 1 } };
+            float temp = 0;
+            float[,] m = new float[3, 1];
+
+            //перемножаем с матрицей
+            for (int p = 0; p < dot_list.Count; p++)
+            {
+                for (int i = 0; i < 1; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        temp = 0;
+                        for (int k = 0; k < 3; k++)
+                        {
+                            temp += dot_list.ElementAt(p)[i, k] * deg_matrix[k, j];
+                        }
+                        m[j, 0] = temp;
+                    }
+                }
+                dot_list_new.Add(new Point((int)m[0, 0], (int)m[1, 0]));
+            }
+
+            g.Clear(Color.White);
+            Point prev = dot_list_new.First();
+            for (int i = 0; i < dot_list_new.Count; i++)
+            {
+                g.DrawLine(_pen, prev, dot_list_new.ElementAt(i));
+                prev = dot_list_new.ElementAt(i);
+            }
+        }
+
+        private void Button_Scale_Click(object sender, EventArgs e)
+        {
+            if (!_drawingObject.IsPoligon())
+            {
+                g.Clear(Color.White);
+                return;
+            }
+            List<float[,]> dot_list = new List<float[,]>();
+            List<Point> dot_list_new = new List<Point>();
+
+            float dist_x = 0;
+            float dist_y = 0;
+
+            float k_x = float.Parse(kx.Text);
+            float k_y = float.Parse(ky.Text);
+            //формируем матрицу-строку координат и матрицу преобразования
+            for (int i = 0; i < _drawingObject.Lines.Count; i++)
+            {
+                dot_list.Add(new float[1, 3] { { _drawingObject.Lines.ElementAt(i).a.X, _drawingObject.Lines.ElementAt(i).a.Y, 1 } });
+                dist_x += _drawingObject.Lines.ElementAt(i).a.X;
+                dist_y += _drawingObject.Lines.ElementAt(i).a.Y;
+            }
+            dist_x = helper.IsEmpty ? dist_x / dot_list.Count : helper.X;
+            dist_y = helper.IsEmpty ? dist_y / dot_list.Count : helper.Y;
+            dot_list.Add(new float[1, 3] { { _drawingObject.Lines.Last().b.X, _drawingObject.Lines.Last().b.Y, 1 } });
+
+            float[,] deg_matrix = new float[3, 3] { {  k_x, 0, 0 },
+                                                    {  0, k_y, 0 },
+            { -k_x*dist_x + dist_x, -k_y*dist_y + dist_y, 1 } };
+            float temp = 0;
+            float[,] m = new float[3, 1];
+
+            //перемножаем с матрицей
+            for (int p = 0; p < dot_list.Count; p++)
+            {
+                for (int i = 0; i < 1; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        temp = 0;
+                        for (int k = 0; k < 3; k++)
+                        {
+                            temp += dot_list.ElementAt(p)[i, k] * deg_matrix[k, j];
+                        }
+                        m[j, 0] = temp;
+                    }
+                }
+                dot_list_new.Add(new Point((int)m[0, 0], (int)m[1, 0]));
+            }
+
+            g.Clear(Color.White);
+            Point prev = dot_list_new.First();
+            for (int i = 0; i < dot_list_new.Count; i++)
+            {
+                g.DrawLine(_pen, prev, dot_list_new.ElementAt(i));
+                prev = dot_list_new.ElementAt(i);
+            }
+        }
+
+        private void Button_SetPoint_Click(object sender, EventArgs e)
+        {
+            _drawingState = DrawingState.DRAWING_DOT;
+        }
+
+        enum DrawingState { NODRAWING, FREE_DRAWING, DRAWING_DOT }
     }
 
     class DrawingObject
@@ -98,7 +286,6 @@ namespace WinFormsApp1
         public bool IsCurve() => Lines.Count > 0 && !Lines.First.Value.a.Equals(Lines.Last.Value.b);
 
         public bool IsPoligon() => Lines.Count > 0 && Lines.First.Value.a.Equals(Lines.Last.Value.b);
-
 
         public bool IsClosable(PointF p) => Lines.Count > 0 && DIST_FOR_CONNECTING >= Math.Sqrt(Math.Pow(p.X - Lines.First.Value.a.X, 2) +
             Math.Pow(p.Y - Lines.First.Value.a.Y, 2));
